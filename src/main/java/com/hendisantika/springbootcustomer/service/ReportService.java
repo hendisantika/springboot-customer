a2029806f9659d8cc1b7668691ec59eee79a2732
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-16 06:15:08 +0700

    #21 - Add exportReport service in ReportService

--- src/main/java/com/hendisantika/springbootcustomer/service/ReportService.java
+++ src/main/java/com/hendisantika/springbootcustomer/service/ReportService.java
@@ -1,9 +1,22 @@
 package com.hendisantika.springbootcustomer.service;
 
+import com.hendisantika.springbootcustomer.model.Customer;
 import com.hendisantika.springbootcustomer.repository.CustomerRepository;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +32,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReportService {
     @Autowired
     private CustomerRepository customerRepository;
+
+    public String exportReport(String reportFormat) throws Exception {
+        // String path = "D:\\Reports";
+        String path = "C:\\Users\\USER\\Downloads\\demo\\src\\main\\resources";
+        List<Customer> customers = customerRepository.findAll();
+        //load file and compile it
+        File file = ResourceUtils.getFile("classpath:Customer.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(customers);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "hameed");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\customer.html");
+            //byte[] bytes = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+        }
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\customer.pdf");
+            // byte[] bytes = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+
+        }
+
+        return "report generated in path : " + path;
+/*        byte[] bytes = Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+		res.getOutputStream().write(contentOf(file));*/
+        //return bytes;
+
+    }
 }
