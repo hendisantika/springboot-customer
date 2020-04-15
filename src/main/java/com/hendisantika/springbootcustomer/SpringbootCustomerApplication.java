568484b3419d81a7c7571c607b520a2c7e19798c
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-16 06:16:51 +0700

    #23 - Add SpringBootServletInitializer

--- src/main/java/com/hendisantika/springbootcustomer/SpringbootCustomerApplication.java
+++ src/main/java/com/hendisantika/springbootcustomer/SpringbootCustomerApplication.java
@@ -2,9 +2,15 @@ package com.hendisantika.springbootcustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootCustomerApplication {
+public class SpringbootCustomerApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+        return app.sources(SpringbootCustomerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootCustomerApplication.class, args);
