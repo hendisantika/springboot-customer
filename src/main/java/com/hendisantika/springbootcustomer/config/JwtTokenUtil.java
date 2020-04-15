a64fa759240b3494217e3e7a768543c73a32ab5c
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-16 06:18:59 +0700

    #24 - Add JwtTokenUtil

---
+++ src/main/java/com/hendisantika/springbootcustomer/config/JwtTokenUtil.java
@@ -0,0 +1,22 @@
+package com.hendisantika.springbootcustomer.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-customer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/04/20
+ * Time: 06.18
+ */
+@Component
+public class JwtTokenUtil {
+
+    public static final long JWT_TOKEN_VALIDITY = 1 * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private String secret;
+}
