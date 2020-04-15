56fc7c32206cc4a858a9b7de6a9ce294d4c7d365
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-16 06:23:42 +0700

    #31 - Add configureGlobal in WebSecurityConfig

--- src/main/java/com/hendisantika/springbootcustomer/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/springbootcustomer/config/WebSecurityConfig.java
@@ -2,10 +2,12 @@ package com.hendisantika.springbootcustomer.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,4 +31,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // configure AuthenticationManager so that it knows from where to load
+        // user for matching credentials
+        // Use BCryptPasswordEncoder
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(new PasswordEncoder() {
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                // TODO Auto-generated method stub
+                return true;
+            }
+
+            @Override
+            public String encode(CharSequence charSequence) {
+                // TODO Auto-generated method stub
+                return charSequence.toString();
+            }
+        });
+    }
+
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+
 }
