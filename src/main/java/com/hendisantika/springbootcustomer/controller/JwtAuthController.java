8be7d7b439bda38410272f9d12c7298ad0425161
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 06:01:41 +0700

    #38 - Add createAuthenticationToken in JwtAuthController

--- src/main/java/com/hendisantika/springbootcustomer/controller/JwtAuthController.java
+++ src/main/java/com/hendisantika/springbootcustomer/controller/JwtAuthController.java
@@ -1,12 +1,18 @@
 package com.hendisantika.springbootcustomer.controller;
 
 import com.hendisantika.springbootcustomer.config.JwtTokenUtil;
+import com.hendisantika.springbootcustomer.dto.JwtRequest;
+import com.hendisantika.springbootcustomer.dto.JwtResponse;
 import com.hendisantika.springbootcustomer.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,6 +37,20 @@ public class JwtAuthController {
 
     @Autowired
     private MyUserDetailsService userDetailsService;
+
+    @PostMapping(value = "/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+        System.out.println(authenticationRequest.toString());
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
 
     private void authenticate(String username, String password) throws Exception {
         try {
