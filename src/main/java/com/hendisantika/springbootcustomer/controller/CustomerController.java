2e44f7cb395066754f9e8a69cf6ad90eb683f311
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 06:03:37 +0700

    #41 - Add getCustomer in CustomerController

--- src/main/java/com/hendisantika/springbootcustomer/controller/CustomerController.java
+++ src/main/java/com/hendisantika/springbootcustomer/controller/CustomerController.java
@@ -3,10 +3,13 @@ package com.hendisantika.springbootcustomer.controller;
 import com.hendisantika.springbootcustomer.model.Customer;
 import com.hendisantika.springbootcustomer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +32,11 @@ public class CustomerController {
     @PostMapping(value = "/insert")
     public void createCustomer(@RequestBody Customer customer) {
         customerService.create(customer);
+    }
+
+    //To get the list of customers
+    @GetMapping(produces = "application/json", value = "/getUsers")
+    public List<Customer> getCustomer() {
+        return customerService.getCustomer();
     }
 }
