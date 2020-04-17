4a347e00bf19d44c3cbd9ef103ebc87b38938497
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-18 06:04:09 +0700

    #43 - Add deleteCustomer in CustomerController

--- src/main/java/com/hendisantika/springbootcustomer/controller/CustomerController.java
+++ src/main/java/com/hendisantika/springbootcustomer/controller/CustomerController.java
@@ -3,7 +3,9 @@ package com.hendisantika.springbootcustomer.controller;
 import com.hendisantika.springbootcustomer.model.Customer;
 import com.hendisantika.springbootcustomer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,5 +47,11 @@ public class CustomerController {
     @PutMapping(value = "/updateUser")
     public Customer updateCustomer(@RequestBody Customer customer) {
         return customerService.update(customer, customer.getId());
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteCustomer(@PathVariable("id") Integer id) {
+
+        customerService.deleteById(id);
     }
 }
